f93bbab21a59d48be82f5a2a372618d1d96ef859
JefersonXD77-hub <aguilapanjoj@gmail.com>
2026-02-03 20:04:54 -0600

    Primer commit

---
+++ src/main/java/ejercicio/Clase.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio;
+
+/**
+ *
+ * @author aguil
+ */
+public class Clase {
+    
+    public static void main(String Args[]){
+    
+        System.out.println("Hola mundo");
+    
+    }
+    
+}
